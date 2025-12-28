d3f3e90d961d1d3388dea20dfb620d7c6db4f9e4
READTalk <redtalk.app@hotmail.com>
2025-12-29 02:00:37 +0700

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -59,13 +59,13 @@ export default {
 				),
 			},
 			theme: {
-				title: "myAuth",
-				primary: "#0051c3",
+				title: "READTalk - Lock",
+				primary: "#ff0000",
 				favicon: "https://workers.cloudflare.com//favicon.ico",
 				logo: {
-					dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
+					dark: "https://readtalk.vercel.app/logo.webp",
 					light:
-						"https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
+						"https://readtalk.vercel.app/logo.webp",
 				},
 			},
 			success: async (ctx, value) => {
