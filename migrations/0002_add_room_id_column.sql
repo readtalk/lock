69b9c9418b43a27bc8ee92697348008868c1b9d0
READTalk <redtalk.app@hotmail.com>
2025-12-29 03:39:15 +0700

    0002_add_room_id_column.sql
    npm run deploy

--- migrations/0002_add_room_id_column.sql
+++ migrations/0002_add_room_id_column.sql
@@ -1,2 +1,4 @@
--- Migration number: 0002 	 2025-01-15
-ALTER TABLE user ADD COLUMN room_id TEXT UNIQUE;
+cat > migrations/0002_add_room_id.sql << 'EOF'
+-- Migration number: 0002 	 2025-12-28
+ALTER TABLE user ADD COLUMN room_id TEXT;
+EOF
