a760e5703350b1de79b2af1f2f049ca19ae890c4
READTalk <redtalk.app@hotmail.com>
2025-12-29 14:03:26 +0700

    Delete migrations/0001_create_user_table.sql

--- migrations/0001_create_user_table.sql
+++
@@ -1,9 +0,0 @@
--- Migration number: 0001 	 2024-12-27T22:04:18.794Z
-CREATE TABLE IF NOT EXISTS user (
-    id TEXT PRIMARY KEY NOT NULL DEFAULT (lower(hex(randomblob(16)))),
-    email TEXT UNIQUE NOT NULL,
-    room_id TEXT UNIQUE,
-    nama TEXT,
-    username TEXT,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
